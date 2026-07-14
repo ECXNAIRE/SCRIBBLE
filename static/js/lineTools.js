ee6d21b4bc3a459c4baacea8d6124dd9fe35800d
suryansh <suryanshsinghrajput007@gmail.com>
2026-07-14 06:21:45 +0530

    finally fixed the handle position

--- static/js/lineTools.js
+++ static/js/lineTools.js
@@ -28,39 +28,82 @@ export function distanceToLine(px, py, x1, y1, x2, y2) {
     const dx = px - closestX
     const dy = py - closestY
 
-    return Math.hypot(dx * dx + dy * dy)
+    return Math.sqrt(dx * dx + dy * dy)
 }
 
 
 export function lineSelectionBox(shape, ctx) {
     const handleSize = 8
-    const padding = 6
+    const padding = 9
 
     const left = Math.min(shape.x1, shape.x2)
     const right = Math.max(shape.x1, shape.x2)
 
     const top = Math.min(shape.y1, shape.y2)
     const bottom = Math.max(shape.y1, shape.y2)
-    
-    ctx.strokeStyle = "#3B82F6"
-    ctx.lineWidth = 1
-    ctx.setLineDash([5, 5])
 
-    ctx.strokeRect(
-        left - padding,
-        top - padding,
-        right - left + padding * 2,
-        bottom - top + padding * 2
+
+    const cx = (shape.x1 + shape.x2) / 2
+    const cy = (shape.y1 + shape.y2) / 2
+
+    const angle = Math.atan2(
+        shape.y2 - shape.y1,
+        shape.x2 - shape.x1
+    )
+
+    const length = Math.hypot(
+        shape.x2 - shape.x1,
+        shape.y2 - shape.y1
     );
 
-    ctx.setLineDash([]);
 
+
+
+    ctx.save()
+    ctx.translate(cx, cy)
+    ctx.rotate(angle)
+
+    ctx.strokeStyle = "#3B82F6"
+    ctx.lineWidth = 1
+
+    ctx.strokeRect(
+        -length / 2 - padding,
+        -padding,
+        length + padding * 2,
+        padding * 2
+    );
+
+    ctx.restore()
+
+
+
+    const dx = shape.x2 - shape.x1;
+    const dy = shape.y2 - shape.y1;
+
+    const len = Math.hypot(dx, dy);
+
+    const ux = dx / len;
+    const uy = dy / len;
+
+    const offset = -9;
 
 
     ctx.fillStyle = "#FFFFFF";
     ctx.strokeStyle = "#3B82F6";
     ctx.lineWidth = 1;
 
-    drawHandle(shape.x1, shape.y1, handleSize, ctx)
-    drawHandle(shape.x2, shape.y2, handleSize, ctx)
+
+    drawHandle(
+        shape.x1 + ux * offset,
+        shape.y1 + uy * offset,
+        handleSize,
+        ctx
+    );
+
+    drawHandle(
+        shape.x2 - ux * offset,
+        shape.y2 - uy * offset,
+        handleSize,
+        ctx
+    );
 }
