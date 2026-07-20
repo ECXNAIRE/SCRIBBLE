c09835913108aa72c640e98822b8c18421790f8a
suryansh <suryanshsinghrajput007@gmail.com>
2026-07-20 09:36:28 +0530

    finally modullarised all the major functions

--- static/js/canvas/renderCurrentShape.js
+++ static/js/canvas/renderCurrentShape.js
@@ -1,1 +1,36 @@
 import { state } from "../state.js";
+import { cacheCanvas, cacheCtx } from "./canvas.js";
+import { camera } from "./cameraFunction.js";
+import { drawShape } from "./drawShapes.js";
+
+
+
+
+export function renderCurrentShape() {
+    cacheCtx.setTransform(
+        camera.zoom,
+        0,
+        0,
+        camera.zoom,
+        -camera.x * camera.zoom,
+        -camera.y * camera.zoom
+    )
+
+    cacheCtx.clearRect(
+        camera.x,
+        camera.y,
+        cacheCanvas.width / camera.zoom,
+        cacheCanvas.height / camera.zoom
+    );
+
+
+    if (state.currentShape) {
+        drawShape(state.currentShape, cacheCtx);
+    } else if (state.isDragging && state.dragShape) {
+        drawShape(state.dragShape, cacheCtx);
+    } else if (state.isResizing && state.selectedShape) {
+        drawShape(state.selectedShape, cacheCtx);
+    }
+
+    cacheCtx.setTransform(1, 0, 0, 1, 0, 0);
+}
